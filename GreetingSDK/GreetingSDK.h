f64f1a2b85d4eea238911daa6a85bf654fc81729
Maris Lagzdins <maris.lagzdins@testdevlab.com>
2020-08-25 10:49:48 +0300

    Initial Commit

---
+++ GreetingSDK/GreetingSDK.h
@@ -0,0 +1,19 @@
+//
+//  GreetingSDK.h
+//  GreetingSDK
+//
+//  Created by Maris Lagzdins on 25/08/2020.
+//  Copyright © 2020 Maris Lagzdins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GreetingSDK.
+FOUNDATION_EXPORT double GreetingSDKVersionNumber;
+
+//! Project version string for GreetingSDK.
+FOUNDATION_EXPORT const unsigned char GreetingSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GreetingSDK/PublicHeader.h>
+
+
